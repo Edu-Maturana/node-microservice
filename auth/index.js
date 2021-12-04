5a26f75f7668a4689878545c50b113ccb697ebc4
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2021-12-03 22:11:10 -0300

    comprobar verificacion con token

--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,52 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const secret = config.jwt.secret;
 
 function sign(data) {
-    return jwt.sign(data, 'secreto')
+    return jwt.sign(data, secret)
+}
+
+function verify(token) {
+    return jwt.verify(token, secret);
+}
+
+const check = {
+    own: function(req, owner) {
+        const decoded = decodeHeader(req);
+        console.log(decoded);
+        if (decoded.id !== owner) {
+            throw new Error('You don\'t have permission to do that');
+        }
+    },
+}
+
+function getToken(authorization) {
+    // Bearer token
+    if (!authorization) {
+        throw new Error('No token provided');
+    }
+
+    if (authorization.indexOf('Bearer') === -1) {
+        throw new Error('Invalid token');
+    }
+
+    let token = authorization.replace('Bearer ', ''); 
+
+    return token;
+}
+
+function decodeHeader(req) {
+    const authorization = req.headers.authorization || '';
+    const token = getToken(authorization);
+    decoded = verify(token);
+
+    req.user = decoded;
+
+    return decoded;
 }
 
 module.exports = {
-    sign
+    sign,
+    check,
 }
